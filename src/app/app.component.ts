d402ea417071064b9f979c4bf23b06ee1a114e41
nathanielpascual <nath_pascual@yahoo.com>
2019-01-02 10:01:58 +0800

    finished observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UsersService} from './users.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy {
+  user1Activated = false;
+  user2Activated = false;
+  userSubscription: Subscription;
+  constructor(private usersService : UsersService) {
+
+  }
+
+  ngOnInit() {
+    this.userSubscription = this.usersService.userActivated
+        .subscribe((id: number)=>{
+            if(id ===1){
+              this.user1Activated = true;
+              this.user2Activated = false;
+            }
+            else {
+              this.user2Activated = true;
+              this.user1Activated = false;
+            }
+        });
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { interval } from 'rxjs';
-import 'rxjs/Rx';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Observer,Subscription,interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,OnDestroy {
+  numberOnSubscription : Subscription;
+  customOnSubscription : Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    const numbers = interval(1000);
-    numbers.subscribe((number:number)=>{
+    const numbers = interval(1000)
+          .pipe(map(
+            (data : number) => { return data * 2}
+          ));
+    this.numberOnSubscription= numbers.subscribe((number:number)=>{
       console.log(number);
     });
-  }
 
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(()=>{
+        observer.next('first package');
+      },2000);
+      setTimeout(()=>{
+        observer.next('second package');
+      },4000);
+      setTimeout(()=>{
+        //observer.error('this does not work');
+        observer.complete;
+      },5000);
+      setTimeout(()=>{
+        observer.next('third package');
+      },6000);
+    });
+
+    this.customOnSubscription = myObservable.subscribe(
+          (data: string)=> { console.log(data)},
+          (error: string)=> { console.log(error)},
+          ()=> { console.log('completed')},
+      );
+  };
+
+  ngOnDestroy() {
+    this.numberOnSubscription.unsubscribe();
+    this.customOnSubscription.unsubscribe();
+  }
 }
